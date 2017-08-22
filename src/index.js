6deea8753ea4dc8a22b9e25dd87fba041cd0eee6
Jason Sutter <jasonsutter87@gmail.com>
2017-08-22 14:28:43 -0500

    Text rendering to the page from API

--- src/index.js
+++ src/index.js
@@ -5,32 +5,38 @@ import './index.css';
 
 
 
-
 class App extends React.Component{
-  performGetWeather(){
-    // return (
-      axios.get('https://api.breezometer.com/baqi', {
-        params: {
-          lat: 30.2688806,
-          lon: -97.7427527,
-          key: 'a3b8b658b30540b19f033f9b5dfe6511',
-        }
-      })
-      .then(function (response) {
-        console.log(response.data);
-        console.log(response.data.breezometer_description);
-        return response.data.breezometer_description;
-      })
-      .catch(function (error) {
-        console.log(error);
-        return error;
-      })
-    // )
+  constructor(){
+    super();
+    this.state={};
+  }
+
+  componentWillMount(){
+    axios.get('https://api.breezometer.com/baqi', {
+      params: {
+        lat: 30.2688806,
+        lon: -97.7427527,
+        key: 'a3b8b658b30540b19f033f9b5dfe6511',
+      }
+    })
+    .then((response) => {
+      this.setState({
+        weather: response.data.breezometer_description,
+      });
+    })
+    .catch(function (error) {
+      console.log(error);
+      return error;
+    })
   }
 
   render() {
     return (
-      <div>{this.performGetWeather()}</div>
+      <div>
+        <h3>
+          Todays air is: {this.state.weather}
+        </h3>
+      </div>
     )
   }
 }
