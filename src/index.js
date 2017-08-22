378b48a297dd3bb212f58cd357b837c6495f81b3
Jason Sutter <jasonsutter@Jasons-MacBook.local>
2017-08-22 07:25:28 -0500

    Get first axios call

--- src/index.js
+++ src/index.js
@@ -4,11 +4,33 @@ import axios from 'axios';
 import './index.css';
 
 
+
+
 class App extends React.Component{
+  performGetWeather(){
+    // return (
+      axios.get('https://api.breezometer.com/baqi', {
+        params: {
+          lat: 30.2688806,
+          lon: -97.7427527,
+          key: 'a3b8b658b30540b19f033f9b5dfe6511',
+        }
+      })
+      .then(function (response) {
+        console.log(response.data);
+        console.log(response.data.breezometer_description);
+        return response.data.breezometer_description;
+      })
+      .catch(function (error) {
+        console.log(error);
+        return error;
+      })
+    // )
+  }
 
   render() {
     return (
-      <h1>hello world</h1>
+      <div>{this.performGetWeather()}</div>
     )
   }
 }
