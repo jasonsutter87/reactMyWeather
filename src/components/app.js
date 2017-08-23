b6cf9cdc8b58ae972adc2a5f88895ec3fe44e010
Jason Sutter <jasonsutter87@gmail.com>
2017-08-23 11:38:39 -0500

    Update app to include searchbar, and swatch

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './searchBar';
+import Swatch from './colorSwatch';
 
 class App extends React.Component{
   render() {
     return (
-      <div>hello world</div>
+      <div>
+      <SearchBar />
+      <Swatch />
+      </div>
     )
   };
 }
