94a3848c9af7936f47f8f62cf017be486c3e82c1
Jason Sutter <jasonsutter87@gmail.com>
2017-08-23 11:18:23 -0500

    Implement Hello World

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 import './styles/index.css';
-import App from './components/App';
+import App from './components/app';
 
 ReactDOM.render(
   <App />,
