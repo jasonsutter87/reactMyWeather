d212a265e39fd8d8d6c8966462306ea71a2534f8
Jason Sutter <jasonsutter87@gmail.com>
2017-08-23 11:39:17 -0500

    Implement a div that shows color

---
+++ src/components/colorSwatch.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Swatch extends React.Component{
+  render() {
+    return (
+      <div id='swatch'>
+      </div>
+    )
+  };
+}
+
+export default Swatch;
